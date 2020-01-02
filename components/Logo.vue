1e0313b5dd646340953f46d81e8f14f5428b7f82
dolfant <louis.cascio@hotmail.fr>
2020-01-02 22:06:46 +0100

    feat(home): vue init

---
+++ components/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <img src="https://www.assurup.com/resources/aws/img/assurup-images/logo-assurup/assurup-licorne-bleu.png">
+</template>
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
